e0d64daa5bf3ac40b28ba4529e332be8adf69f4e
Michal Jankowski <rivivias@gmail.com>
2023-11-12 17:41:27 +0100

    complete version

--- main.go
+++ main.go
@@ -64,8 +64,6 @@ func run() {
 	y1 := lineHeight+5
 
 	
-
-	
 	var draw_result_x []float64
 	var draw_result_y []float64
 	
@@ -77,17 +75,16 @@ func run() {
 		
 	}
 	
+	var x float64
+	var y float64
 
 	i := 0
 	for !win.Closed() {
 		win.Clear(color.Black)
-
 		d:=30.0
-		
 		imd.Clear()
-		var x float64
-		var y float64
-		if(y_result[i]>0){
+		
+		if(i<len(x_result)){
 			x =  draw_result_x[i] + 3
 			y =  draw_result_y[i] +3
 		
@@ -126,8 +123,6 @@ func run() {
 		imd.Line(5)
 		
 		
-
-
 		imd.Draw(win)
 
 		win.Update()
@@ -135,18 +130,13 @@ func run() {
 		
 		time.Sleep(time.Second/100) 
 		
-		
-		
 	}
-	
-	plot_results(t_result,x_result,y_result,ax_result)
 }
 
 
 
 
 
-var h = math.Pow(10,-2)
 
 
 type Gravity_Input struct{
@@ -245,13 +235,13 @@ func parameterWindow () {
 				fmt.Println("Erorr: ",err)
 			}
 			gravity_sources = append(gravity_sources, Gravity_Source{
-				mass: mass,
+				mass: 1000000 * mass,
 				x: x,
 				y: y,
 			})
 		}
 		
-		x_result,y_result,t_result=simulation(g,m,u,b,alfa,v)
+		simulation(g,m,u,b,alfa,v)
 		
 		w.Close()
 		app.Quit()
@@ -260,7 +250,7 @@ func parameterWindow () {
 		
 		input_number := strconv.Itoa(len(gravity_sources)+1)
 		input_gravity :=widget.NewEntry()
-		input_gravity_label := widget.NewLabel(input_number+" Mass [kg]")
+		input_gravity_label := widget.NewLabel(input_number+"1M * Mass [kg]")
 		input_x :=widget.NewEntry()
 		input_x_label := widget.NewLabel(input_number+" X[m]")
 		input_y :=widget.NewEntry()
@@ -298,9 +288,11 @@ func parameterWindow () {
 	w.ShowAndRun()
 }
 
-func simulation(g,m,u,b,alfa,v float64) ([]float64,[]float64,[]float64){
-	x_prev := 0.0
-	y_prev := height
+func simulation(g,m,u,b,alfa,v float64) (){
+	h := math.Pow(10,-2)
+	meters_to_pixels := lineHeight/height
+	x_prev := 0.0 
+	y_prev := height 
 	vx_prev := v*math.Cos(alfa)
 	vy_prev:= -v*math.Sin(alfa)
 	Fx_sources := 0.0
@@ -329,8 +321,12 @@ func simulation(g,m,u,b,alfa,v float64) ([]float64,[]float64,[]float64){
 	ax_result = append(ax_result, ax_prev)
 	ay_result = append(ay_result, ay_prev)
 
-	for i:=1;y_prev>0;i++{
+	fmt.Println(y_prev*meters_to_pixels)
+	fmt.Println(x_prev*meters_to_pixels)
+
+	for i:=1;y_prev>=0 && y_prev*meters_to_pixels<=600 && x_prev>=0 && x_prev*meters_to_pixels<=800;i++{
 		x := x_prev + h*vx_prev + math.Pow(h,2)/2*ax_prev
+
 		y := y_prev + h*vy_prev + math.Pow(h,2)/2*ay_prev
 		vx := vx_prev + h*ax_prev
 		vy := vy_prev + h*ay_prev
@@ -342,18 +338,18 @@ func simulation(g,m,u,b,alfa,v float64) ([]float64,[]float64,[]float64){
 			Fx_sources += F*(gravity_source.x-x)/r 
 			Fy_sources += F*(gravity_source.y-y)/r 
 		}
-		Fx = -b*(math.Pow(vx,2)+math.Pow(vy,2))*math.Cos(alfa) + Fx_sources
-		Fy = b*(math.Pow(vx,2)+math.Pow(vy,2))*math.Sin(alfa) - m*g +Fy_sources
+		Fx = -b*(math.Pow(vx,2)+math.Pow(vy,2))*vx/(math.Pow(vx,2)+math.Pow(vy,2)) + Fx_sources
+		Fy = -b*(math.Pow(vx,2)+math.Pow(vy,2))*vy/(math.Pow(vx,2)+math.Pow(vy,2)) - m*g +Fy_sources
 		if y<=(-math.Tan(alfa)*x+height+0.01) && y>=(-math.Tan(alfa)*x+height-0.01){
 			diff := Fy*math.Cos(alfa)+Fx*math.Sin(alfa)
 			if diff<0{
 				Fx = Fx -diff*math.Sin(alfa)+diff*u*math.Cos(alfa) 
 				Fy = Fy -diff*math.Cos(alfa) - diff*u*math.Sin(alfa)
 			}
+		}else if y<(-math.Tan(alfa)*x+height-0.01){
+			vy = -vy
 		}
 		
-		
-		// fmt.Println(float64(i)*h)
 
 		ax := Fx/m
 		ay := Fy/m
@@ -373,45 +369,18 @@ func simulation(g,m,u,b,alfa,v float64) ([]float64,[]float64,[]float64){
 		ax_prev = ax 
 		ay_prev =ay
 	}
-	return x_result,y_result,t_result
-
-	// x := float64(0) 
-	// x_prev := float64(0) 
-	// v:=float64(0) 
-	// v_prev := float64(0) 
-	// a:=float64(0) 
-	// a_prev :=float64(0) 
-	// var x_result []float64
-	// var v_result []float64
-	// var a_result []float64
-	// var t_result []float64
-
-	// for i := 0; x*math.Sin(alfa)< height; i++ {
-	// 	x = x_prev + h*v_prev+math.Pow(h,2)/2*a_prev
-	// 	v = v_prev + h*a_prev
-	// 	a = -b/m*math.Pow(v_prev,2)-g*u*math.Cos(alfa)+g*math.Sin(alfa)
-	// 	x_result = append(x_result,x)
-	// 	v_result = append(v_result,v)
-	// 	a_result = append(a_result,a)
-	// 	t_result = append(t_result,float64(i)*h)
-	// 	x_prev=x
-	// 	v_prev = v
-	// 	a_prev = a
-	// }
-	// return x_result,v_result,a_result,t_result
+	
 }
 
 
 
 
-func plot_results(t_result,x_result,v_result,a_result []float64) {
+func plot_xy(t_result,x_result,y_result[]float64) {
 
-	// Tworzenie nowego wykresu
 	p1 := plot.New()
 	p2 := plot.New()
 	p3 := plot.New()
 
-	// Tworzenie serii danych
 	lineData1 := make(plotter.XYs, len(t_result))
 	for i := range t_result {
 		lineData1[i].X = t_result[i]
@@ -420,7 +389,7 @@ func plot_results(t_result,x_result,v_result,a_result []float64) {
 	lineData2 := make(plotter.XYs, len(t_result))
 	for i := range t_result {
 		lineData2[i].X = t_result[i]
-		lineData2[i].Y = v_result[i]
+		lineData2[i].Y = y_result[i]
 	}
 	lineData3 := make(plotter.XYs, len(t_result))
 	for i := range t_result {
@@ -444,13 +413,13 @@ func plot_results(t_result,x_result,v_result,a_result []float64) {
 	}
 	p1.Add(line1)
 	p1.X.Label.Text = "Time [s]"
-	p1.Y.Label.Text = "Distance [m]"
+	p1.Y.Label.Text = "X [m]"
 	p2.Add(line2)
 	p2.X.Label.Text = "Time [s]"
-	p2.Y.Label.Text = "Velocity [m/s]"
+	p2.Y.Label.Text = "Y [m]"
 	p3.Add(line3)
-	p3.X.Label.Text = "Time [s]"
-	p3.Y.Label.Text = "Acceleration [m/s^2]"
+	p3.X.Label.Text = "X [m]"
+	p3.Y.Label.Text = "Y [m]"
 
 
 
@@ -466,8 +435,87 @@ func plot_results(t_result,x_result,v_result,a_result []float64) {
 	img := vgimg.New(vg.Points(500), vg.Points(600))
     dc := draw.New(img)
 
-	
 
+	t := draw.Tiles{
+        Rows: 3,
+        Cols: 1,
+    }
+
+	canvases := plot.Align(plots, t, dc)
+	plots[0][0].Draw(canvases[0][0])
+	plots[1][0].Draw(canvases[1][0])
+	plots[2][0].Draw(canvases[2][0])
+
+	w, err := os.Create("xy.png")
+    if err != nil {
+        panic(err)
+    }
+
+    png := vgimg.PngCanvas{Canvas: img}
+    if _, err := png.WriteTo(w); err != nil {
+        panic(err)
+    }
+}
+
+func plot_v(t_result,vx_result,vy_result[]float64) {
+
+	p1 := plot.New()
+	p2 := plot.New()
+	p3 := plot.New()
+
+	lineData1 := make(plotter.XYs, len(t_result))
+	for i := range t_result {
+		lineData1[i].X = t_result[i]
+		lineData1[i].Y = vx_result[i]
+	}
+	lineData2 := make(plotter.XYs, len(t_result))
+	for i := range t_result {
+		lineData2[i].X = t_result[i]
+		lineData2[i].Y = vy_result[i]
+	}
+	lineData3 := make(plotter.XYs, len(t_result))
+	for i := range t_result {
+		lineData3[i].X = t_result[i]
+		lineData3[i].Y = math.Sqrt(math.Pow(vx_result[i],2)+math.Pow(vy_result[i],2))
+	}
+	line1, err := plotter.NewLine(lineData1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	line2, err := plotter.NewLine(lineData2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	line3, err := plotter.NewLine(lineData3)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	p1.Add(line1)
+	p1.X.Label.Text = "Time [s]"
+	p1.Y.Label.Text = "Vx [m/s]"
+	p2.Add(line2)
+	p2.X.Label.Text = "Time [s]"
+	p2.Y.Label.Text = "Vy [m/s]"
+	p3.Add(line3)
+	p3.X.Label.Text = "Time [s]"
+	p3.Y.Label.Text = "V [m/s]"
+
+
+
+	plots := make([][]*plot.Plot, 3)
+	plots[0] =make([]*plot.Plot, 1)
+	plots[1] = make([]*plot.Plot, 1)
+	plots[2] = make([]*plot.Plot, 1)
+	plots[0][0] = p1
+	plots[1][0] = p2
+	plots[2][0] = p3
+
+
+	img := vgimg.New(vg.Points(500), vg.Points(600))
+    dc := draw.New(img)
 
 
 	t := draw.Tiles{
@@ -480,7 +528,89 @@ func plot_results(t_result,x_result,v_result,a_result []float64) {
 	plots[1][0].Draw(canvases[1][0])
 	plots[2][0].Draw(canvases[2][0])
 
-	w, err := os.Create("plot.png")
+	w, err := os.Create("v.png")
+    if err != nil {
+        panic(err)
+    }
+
+    png := vgimg.PngCanvas{Canvas: img}
+    if _, err := png.WriteTo(w); err != nil {
+        panic(err)
+    }
+}
+
+func plot_a(t_result,ax_result,ay_result[]float64) {
+
+	p1 := plot.New()
+	p2 := plot.New()
+	p3 := plot.New()
+
+	lineData1 := make(plotter.XYs, len(t_result))
+	for i := range t_result {
+		lineData1[i].X = t_result[i]
+		lineData1[i].Y = ax_result[i]
+	}
+	lineData2 := make(plotter.XYs, len(t_result))
+	for i := range t_result {
+		lineData2[i].X = t_result[i]
+		lineData2[i].Y = ay_result[i]
+	}
+	lineData3 := make(plotter.XYs, len(t_result))
+	for i := range t_result {
+		lineData3[i].X = t_result[i]
+		lineData3[i].Y = math.Sqrt(math.Pow(ax_result[i],2)+math.Pow(ay_result[i],2))
+	}
+	line1, err := plotter.NewLine(lineData1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	line2, err := plotter.NewLine(lineData2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	line3, err := plotter.NewLine(lineData3)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	p1.Add(line1)
+	p1.X.Label.Text = "Time [s]"
+	p1.Y.Label.Text = "Ax [m/s]"
+	p2.Add(line2)
+	p2.X.Label.Text = "Time [s]"
+	p2.Y.Label.Text = "Ay [m/s]"
+	p3.Add(line3)
+	p3.X.Label.Text = "Time [s]"
+	p3.Y.Label.Text = "A [m/s]"
+
+
+
+	plots := make([][]*plot.Plot, 3)
+	plots[0] =make([]*plot.Plot, 1)
+	plots[1] = make([]*plot.Plot, 1)
+	plots[2] = make([]*plot.Plot, 1)
+	plots[0][0] = p1
+	plots[1][0] = p2
+	plots[2][0] = p3
+
+
+	img := vgimg.New(vg.Points(500), vg.Points(600))
+    dc := draw.New(img)
+
+
+	t := draw.Tiles{
+        Rows: 3,
+        Cols: 1,
+    }
+
+	canvases := plot.Align(plots, t, dc)
+	plots[0][0].Draw(canvases[0][0])
+	plots[1][0].Draw(canvases[1][0])
+	plots[2][0].Draw(canvases[2][0])
+
+	w, err := os.Create("a.png")
     if err != nil {
         panic(err)
     }
@@ -496,5 +626,8 @@ func plot_results(t_result,x_result,v_result,a_result []float64) {
 func main () {
 	parameterWindow()
 	pixelgl.Run(run)
+	plot_xy(t_result,x_result,y_result)
+	plot_v(t_result,vx_result,vy_result)
+	plot_a(t_result,ax_result,ay_result)
 }
 
